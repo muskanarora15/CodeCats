463661dc2109577914080354ec48340652678eb2
muskanarora15 <92794356+muskanarora15@users.noreply.github.com>
2021-11-12 15:50:29 +0530

    Create ques7.c

---
+++ week4/ques7.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+    int x,j,i,t;
+     printf("\nenter number of testcase\n");
+   scanf("%d",&t);
+  
+for(i=1;i<=t;i++){
+ int m=1;
+  printf("enter intial cordinate and number of jumps\n");
+   scanf("%d\n%d",&x,&j);
+  
+    while(m<=j)
+    {
+        if(x&1)
+        {
+            x = x+m;
+            m++;
+          
+        }
+        else
+        {
+              x = x-m;
+            m++;
+            
+    }
+    }
+    printf("\n final coordinate is %d\n",x);
+}
+}
